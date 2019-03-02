05b0fee93e03c8d1a454a463724d0c5e373c8cfa
RCBelmont <dbelmont16@gmail.com>
2019-03-02 23:21:50 +0800

    1.增加旋转角度控制
    Signed-off-by: RCBelmont <dbelmont16@gmail.com>

--- SHLightGenerator/Assets/Proj/SHDataGen/Editor/SHDataGenerator.cs
+++ SHLightGenerator/Assets/Proj/SHDataGen/Editor/SHDataGenerator.cs
@@ -2,90 +2,93 @@
 //Created By RCBelmont on 2019年3月1日
 //Description SH数据生成器面板
 //============================
+
 using UnityEditor;
 using UnityEngine;
+
 namespace RCBelmont.SHLightGenerator
 {
-   public class SHDataGenerator : EditorWindow
-{
-    private Cubemap _map;
-    private Color _skyColor, _equatorColor, _groundColor;
-    private SHData _shData;
-    private Material _mat;
-
-    private enum AmbientType
+    public class SHDataGenerator : EditorWindow
     {
-        SkyBox,
-        TriLight
-    }
+        private Cubemap _map;
+        private Color _skyColor, _equatorColor, _groundColor;
+        private SHData _shData;
+        private Material _mat;
+        private float _rotate;
 
-    private AmbientType _aType = AmbientType.SkyBox;
-
-    [MenuItem("Tools/SHLightDataGenerator")]
-    public static void OpenWindow()
-    {
-        EditorWindow win = GetWindow<SHDataGenerator>();
-        win.minSize = new Vector2(300, 500);
-        win.minSize = new Vector2(300, 500);
-        win.name = "SHLightGenerator";
-        win.Show();
-    }
-
-
-    void OnGUI()
-    {
-        _aType = (AmbientType) EditorGUILayout.EnumPopup("AmbientType", _aType);
-        switch (_aType)
+        private enum AmbientType
         {
-            case AmbientType.SkyBox:
-                _map = (Cubemap) EditorGUILayout.ObjectField("EnvTex: ", _map, typeof(Cubemap), false);
-                if (GUILayout.Button("Generate!"))
-                {
-                    _shData = SHLightGeneratorLib.GenSHBySkyBox(_map, 0);
-                }
-
-                break;
-            case AmbientType.TriLight:
-                _skyColor = EditorGUILayout.ColorField("Sky", _skyColor);
-                _equatorColor = EditorGUILayout.ColorField("Equator", _equatorColor);
-                _groundColor = EditorGUILayout.ColorField("Ground", _groundColor);
-                if (GUILayout.Button("Generate!"))
-                {
-                    _shData = SHLightGeneratorLib.GenSHByTriLight(_skyColor, _equatorColor, _groundColor);
-                }
-
-                break;
+            SkyBox,
+            TriLight
         }
 
-        EditorGUILayout.Space();
-        if (_shData)
+        private AmbientType _aType = AmbientType.SkyBox;
+
+        [MenuItem("Tools/SHLightDataGenerator")]
+        public static void OpenWindow()
         {
-            GUILayout.Label("SHAr: " + _shData.SHAr);
-            GUILayout.Label("SHAg: " + _shData.SHAg);
-            GUILayout.Label("SHAb: " + _shData.SHAb);
-            GUILayout.Label("SHBr: " + _shData.SHAr);
-            GUILayout.Label("SHBg: " + _shData.SHAg);
-            GUILayout.Label("SHBb: " + _shData.SHAb);
-            GUILayout.Label("SHC: " + _shData.SHC);
-            if (GUILayout.Button("SaveSHData"))
+            EditorWindow win = GetWindow<SHDataGenerator>();
+            win.minSize = new Vector2(300, 500);
+            win.minSize = new Vector2(300, 500);
+            win.name = "SHLightGenerator";
+            win.Show();
+        }
+
+
+        void OnGUI()
+        {
+            _aType = (AmbientType) EditorGUILayout.EnumPopup("AmbientType", _aType);
+          
+            switch (_aType)
             {
-                string path = EditorUtility.SaveFilePanel("SaveSHData", Application.dataPath, "SHData", "asset");
-                if (!string.IsNullOrEmpty(path))
-                {
-                    if (!path.Contains(Application.dataPath))
+                case AmbientType.SkyBox:
+                    _map = (Cubemap) EditorGUILayout.ObjectField("EnvTex: ", _map, typeof(Cubemap), false);
+                    _rotate = EditorGUILayout.Slider("EnvTexRotate", _rotate, 0, 360);
+                    if (GUILayout.Button("Generate!"))
                     {
-                        Debug.LogError("Invalid Path");
-                        return;
+                        _shData = SHLightGeneratorLib.GenSHBySkyBox(_map, _rotate);
                     }
 
-                    path = "Assets" + path.Replace(Application.dataPath, "");
-                    AssetDatabase.CreateAsset(_shData, path);
-                    Selection.activeObject = _shData;
+                    break;
+                case AmbientType.TriLight:
+                    _skyColor = EditorGUILayout.ColorField("Sky", _skyColor);
+                    _equatorColor = EditorGUILayout.ColorField("Equator", _equatorColor);
+                    _groundColor = EditorGUILayout.ColorField("Ground", _groundColor);
+                    if (GUILayout.Button("Generate!"))
+                    {
+                        _shData = SHLightGeneratorLib.GenSHByTriLight(_skyColor, _equatorColor, _groundColor);
+                    }
+
+                    break;
+            }
+
+            EditorGUILayout.Space();
+            if (_shData)
+            {
+                GUILayout.Label("SHAr: " + _shData.SHAr);
+                GUILayout.Label("SHAg: " + _shData.SHAg);
+                GUILayout.Label("SHAb: " + _shData.SHAb);
+                GUILayout.Label("SHBr: " + _shData.SHAr);
+                GUILayout.Label("SHBg: " + _shData.SHAg);
+                GUILayout.Label("SHBb: " + _shData.SHAb);
+                GUILayout.Label("SHC: " + _shData.SHC);
+                if (GUILayout.Button("SaveSHData"))
+                {
+                    string path = EditorUtility.SaveFilePanel("SaveSHData", Application.dataPath, "SHData", "asset");
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        if (!path.Contains(Application.dataPath))
+                        {
+                            Debug.LogError("Invalid Path");
+                            return;
+                        }
+
+                        path = "Assets" + path.Replace(Application.dataPath, "");
+                        AssetDatabase.CreateAsset(_shData, path);
+                        Selection.activeObject = _shData;
+                    }
                 }
-               
             }
         }
     }
-} 
-
 }
